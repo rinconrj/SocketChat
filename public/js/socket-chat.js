4e3cc1462935e887c059a460d1b785ab0acd4a5f
rinconrj <rinconrj@gmail.com>
2019-01-31 12:46:46 -0200

    first commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,57 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if(!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesario')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('EntrarChat', usuario, function(resp) {
+      console.log('los usuarios conectados', resp);
+    })
+})
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// // Enviar información
+// socket.emit('crearMensaje', {
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+socket.on('listaPersona', function(mensaje) {
+
+  console.log(mensaje);
+
+});
+
+//mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+  console.log('mensaje Privado: ', mensaje);
+
+})
